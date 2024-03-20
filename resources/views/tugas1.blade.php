4fd01464908b39ee42d7d2367fa2fb3fc983c134
KfbySyrz <fabisyairozie@student.ub.ac.id>
2024-03-20 14:47:07 +0700

    first commit

---
+++ resources/views/tugas1.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Tugas 1 - Biodata</title>
+</head>
+
+<body>
+    <h1> Biodata </h1>
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>: A. Kafaby Syairozie</td>
+        </tr>
+        <tr>
+            <td>NIM</td>
+            <td>: 215150401111028</td>
+        </tr>
+        <tr>
+            <td>Alamat</td>
+            <td>: Jl. Polowijen 1</td>
+        </tr>
+        <tr>
+            <td>Hobi</td>
+            <td>: Belajar</td>
+        </tr>
+
+    </table>
+</body>
+
+</html>
